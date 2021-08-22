216925d95788ae8ff9810e4972500ec5f6715e33
RevandDev <82688238+RevandDev@users.noreply.github.com>
2021-08-22 12:25:37 +0800

    Create setrank.js

---
+++ command/setrank.js
@@ -0,0 +1,13 @@
+module.exports = ({
+  
+ name: "setrank",
+ usage: "setrank <channel>",
+ description: "settings the levelup channel",
+ code: `$description[Rank channel has been set up to <#$mentionedChannels[1;yes]>]
+$color[01ff00]
+$setServerVar[rch;$mentionedChannels[1;yes]]
+$setServerVar[rsystem;1]
+$onlyBotPerms[mentioneveryone;{description:I dont have permission \`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+})
