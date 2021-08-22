41bb12854de6db6de1a9002ed565b2107ab3fb6c
RevandDev <82688238+RevandDev@users.noreply.github.com>
2021-08-22 12:26:24 +0800

    Create help.js

---
+++ command/help.js
@@ -0,0 +1,12 @@
+module.exports = {
+  
+  name: "help",
+  code: `
+$color[#b56310]
+$author[Command List;$userAvatar[$clientID]]
+$description[Here is the list of commands!\nYou can use command using \`$getServerVar[prefix]commands\`]
+$addField[:video_game: Level; \`rank\` \`setrank\` \`resetrank\` \`setrankmsg\`]
+$addField[:wrench: Moderation; \`say\` \`kick\` \`ban\` \`avatar\`]
+$footer[Copyright Revand Developer]
+`
+}
