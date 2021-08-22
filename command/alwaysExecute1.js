580ca399f93666fb336bfce0618791e5aad052ad
RevandDev <82688238+RevandDev@users.noreply.github.com>
2021-08-22 12:36:55 +0800

    Create alwaysExecute1.js

---
+++ command/alwaysExecute1.js
@@ -0,0 +1,9 @@
+module.exports = ({
+ name: "$alwaysExecute",
+ code: `$useChannel[$getServerVar[rch]]
+$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$userTag];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+$setUserVar[lvl;$sum[$getUserVar[lvl];1]]
+$setUserVar[rexp;$multi[$getUserVar[rexp];2]]
+$onlyIf[$getUserVar[exp]>=$getUserVar[rexp];]
+$onlyForServers[$guildID;]` 
+})
