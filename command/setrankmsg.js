cf52cc5afc489dad814c0f46181171374d685410
RevandDev <82688238+RevandDev@users.noreply.github.com>
2021-08-22 12:30:49 +0800

    Create setrankmsg.js

---
+++ command/setrankmsg.js
@@ -0,0 +1,21 @@
+module.exports = ({
+ name: "setrankmsg",
+ usage: "setrankmsg <message>",
+ description: "message for the leveled up",
+ code: `$description[You have been setted the message to:
+\`$message\`]
+$color[RANDOM]
+$setServerVar[rmsg;$message]
+$onlyIf[$message!=;You can also use this variables:
+\`\`\`
+{user.tag} = $userTag
+{user.mention} = <@$authorID>
+{level} = 1
+{exp} = 25
+\`\`\`
+Current msg is:
+\`$getServerVar[rmsg]\`]
+$onlyBotPerms[mentioneveryone;managemessages;{description:I need permission \`MANAGE_MESSAGES\`/\`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+})
